33ff254434ade43178fd3f21da4349c27dddb794
xv-zv <sv.yos.n@gmail.com>
2025-12-16 16:04:17 -0500

    update reader.js

--- Utils/reader.js
+++ Utils/reader.js
@@ -1,7 +1,5 @@
-import { recognize } from 'tesseract.js';
+import tesseract from 'tesseract.js';
 import { exists } from './funcs.js';
-
-import fs from 'node:fs/promises';
 
 export async function reader(source, lang = 'spa') {
    const payload = (Buffer.isBuffer(sourve) || (await exists(source))) ? source : null
@@ -11,7 +9,7 @@ export async function reader(source, lang = 'spa') {
       text: 'invalid parameter'
    }
    
-   const result = await recognize(payload, lang)
+   const result = await tesseract.recognize(payload, lang)
    
    return {
       status: true,

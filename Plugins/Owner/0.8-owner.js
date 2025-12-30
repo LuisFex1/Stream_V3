46747ef6cb9f8514e7e02b9db7e8ad0fac1fbd79
xv-zv <sv.yos.n@gmail.com>
2025-12-30 09:19:52 -0500

    update 0.8-owner.js

--- Plugins/Owner/0.8-owner.js
+++ Plugins/Owner/0.8-owner.js
@@ -1,6 +1,25 @@
+import { OPC_CONFIG } from 'wa-sock';
+
 export default {
    cmd: 'menu',
-   func(m){
+   func(m) {
+      const cmds = Object.keys(this.cmds)
+      const prefix = JSON.stringify(OPC_CONFIG.prefix)
       
-   }
+      let text = `
+      Hola @${m.id.split('@')[0]} , aqui tienes la lista de comandos disponible.
+      
+      > *𝌆 「 INFORMACIÓN DEL BOT 」*
+      
+         • *Nombre*: ${this.bot.user.name}
+         • *Version*: 3.0.0
+         • *Prefijo*: ${prefix}
+         
+      > *𝌆 「 COMANDOS 」* 
+      `.trim()
+      
+      text += cmds.map(i => `\n    ➛ \`\`\`${i}\`\`\``).join('')
+      m.reply(text, { mentions: m.id })
+   },
+   isOwner: true 
 }
